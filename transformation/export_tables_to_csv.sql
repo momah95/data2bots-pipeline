549d127aa2142b73f96547af6f1b4f7bfb338d73
Somto Richard Momah <somtomomah1@gmail.com>
2022-10-25 12:19:32 +0100

    psql commands

---
+++ transformation/export_tables_to_csv.sql
@@ -0,0 +1,3 @@
+\copy somtmoma5254_analytics.agg_public_holiday to 'C:/Users/Public/agg_public_holiday.csv' with (DELIMITER ',', FORMAT CSV, HEADER)
+\copy somtmoma5254_analytics.agg_shipments to 'C:/Users/Public/agg_shipments.csv' with (DELIMITER ',', FORMAT CSV, HEADER)
+\copy somtmoma5254_analytics.best_performing_product to 'C:/Users/Public/best_performing_product.csv' with (DELIMITER ',', FORMAT CSV, HEADER)
